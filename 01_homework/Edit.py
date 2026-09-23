ae4850f2d979d7f59e8137979ce224a76d9388f5
Luisa Sophie Schnabel <luisasophie0903@outlook.de>
2026-09-23 23:21:44 +0200

    add picture to first html document

---
+++ 01_homework/Edit.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+print(pd.__version__)
+print(np.__version__)
+
+my_array = np.array([1,2,3,4])
+average = np.mean(my_array)
+print(average) # should print 2.5
