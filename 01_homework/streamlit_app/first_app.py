03e5a404843688be4baebef085ac85017ff1fe02
Luisa Sophie Schnabel <luisasophie0903@outlook.de>
2026-09-23 23:26:30 +0200

    add picture to the first streamlit app

--- 01_homework/streamlit_app/first_app.py
+++ 01_homework/streamlit_app/first_app.py
@@ -2,3 +2,4 @@ import streamlit as st
 
 st.title("First Streamlit App")
 st.write("This is my first app and I am still trying to figure out how everything works.")
+st.image("Bild_UZH.jpg", caption="Main building of UZH:")
